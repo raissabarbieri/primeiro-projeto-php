07ce653c211583a7576a37964411c802ee1ad89d
raissabarbieri <103955940+raissabarbieri@users.noreply.github.com>
2023-03-02 09:52:12 -0300

    Add files via upload

---
+++ Primeiro Projeto/Index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro projeto</title>
+</head>
+<body>
+
+    <h1><?php echo "Ola mundo";?></h1>
+
+    <h2> 8+8= <?php echo(8+8)?></h2>
+    <h2> 8/8= <?php echo(8/8)?></h2>
+    <h2> 8*8= <?php echo(8*8)?></h2>
+    <h2> 8-8= <?php echo(8-8)?></h2>
+    
+</body>
+</html>
